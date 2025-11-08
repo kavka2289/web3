46d2b2be0ccab46ffa0a963f99b4d2dd42b91304
kavka <ruslanvak2@gmail.com>
2025-11-09 00:55:34 +0300

    added level C

--- main/task1.py
+++ main/task1.py
@@ -53,9 +53,17 @@ def get_balance_batch(user_input):
         balance.append(result)
     return balance
 
+def get_top(n, addresses):
+    get_addresses = get_balance_batch(addresses)
+    sorted_strings = sorted(get_addresses, key=lambda s: float(s.split()[0]), reverse=True)
+    result = []
+    for i in range(n):
+        result.append(sorted_strings[i])
+    return result
+
 def main():
     user_input = 'get_balance_batch ["0x51f1774249Fc2B0C2603542Ac6184Ae1d048351d", "0x4830AF4aB9cd9E381602aE50f71AE481a7727f7C"]'
-    result = get_balance_batch(user_input)
+    result = (get_top(2, user_input))
     print(result)
 if __name__ == "__main__":
     main()
